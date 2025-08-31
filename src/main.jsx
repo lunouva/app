cb1f69521ed85902237e100172abe4b9f9509685
lunouva <darkworldkyle@gmail.com>
2025-08-31 17:25:26 -0400

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import "./index.css";   // 👈 add this line
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
